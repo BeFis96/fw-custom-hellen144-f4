fcff6f94d3bb2a4292535a33fb8ccbc36a109065
GitHub git update Action <action@github.com>
2026-07-20 18:34:21 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_RusEFI_Ford_Laser_KJ_1800.h
+++ generated/controllers/generated/signature_RusEFI_Ford_Laser_KJ_1800.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1667284464
-#define TS_SIGNATURE "rusEFI main.2026.07.19.RusEFI_Ford_Laser_KJ_1800.1667284464"
+#define SIGNATURE_HASH 4246491304
+#define TS_SIGNATURE "rusEFI main.2026.07.20.RusEFI_Ford_Laser_KJ_1800.4246491304"
