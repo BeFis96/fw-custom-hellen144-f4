5c1673e3a779de0276afa37e81c1f8db92d71362
GitHub git update Action <action@github.com>
2026-09-12 18:43:26 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_RusEFI_Ford_Laser_KJ_1800.h
+++ generated/controllers/generated/rusefi_generated_RusEFI_Ford_Laser_KJ_1800.h
@@ -2291,7 +2291,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.09.11.RusEFI_Ford_Laser_KJ_1800.3642881488"
+#define TS_SIGNATURE "rusEFI main.2026.09.12.RusEFI_Ford_Laser_KJ_1800.3642881488"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
